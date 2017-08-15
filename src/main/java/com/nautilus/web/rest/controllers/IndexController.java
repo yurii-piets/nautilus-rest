5d71da77000fe7492d62099153bdbed7ab5b4a3f
Yurii Piets <yurii.piets@gmail.com>
2017-08-15 16:45:33 +0200

    controller for test

---
+++ src/main/java/com/nautilus/web/rest/controllers/IndexController.java
@@ -0,0 +1,16 @@
+package com.nautilus.web.rest.controllers;
+
+import com.nautilus.algorithm.MD5;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/index")
+public class IndexController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String index(){
+        return "hello";
+    }
+}
