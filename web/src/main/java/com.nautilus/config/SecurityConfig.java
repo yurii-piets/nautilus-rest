19f92e40e08c2a33a88b5a04bfc81b91b3f0e450
Yurii Piets <yurii.piets@gmail.com>
2017-10-06 10:15:48 +0200

    Spring Security works

--- web/src/main/java/com.nautilus/config/SecurityConfig.java
+++ web/src/main/java/com.nautilus/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.nautilus.config;
 import com.nautilus.rest.mapping.MappingProperties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -15,15 +16,30 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private MappingProperties properties;
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    protected void configure(AuthenticationManagerBuilder auth)
+            throws Exception {
+        auth
+            .inMemoryAuthentication()
+                    .withUser("admin").password("admin").roles("ADMIN")
+                .and()
+                    .withUser("temporary").password("temporary").roles("USER");
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
         String indexMapping = properties.getIndex();
-        String loginMapping = properties.getUserLogin();
-        String registerUser = properties.getUserRegister();
+        String registerUserMapping = properties.getUserRegister();
+        String updateUserMapping = properties.getUserUpdate();
+        String carFoundMapping = properties.getCarFound();
 
         http
-            .authorizeRequests()
-                .antMatchers(indexMapping, loginMapping, registerUser).permitAll()
-                .anyRequest().authenticated()
-            .and().csrf().disable();
+                .authorizeRequests()
+                .antMatchers(carFoundMapping).authenticated()
+                .antMatchers(indexMapping, registerUserMapping, updateUserMapping).permitAll()
+                .anyRequest().permitAll()
+            .and()
+                .httpBasic()
+            .and()
+                .csrf().disable();
     }
 }
