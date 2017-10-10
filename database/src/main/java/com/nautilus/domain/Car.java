b6a317702125c3ca4917d203c37578b081dda6d0
Yurii Piets <yurii.piets@gmail.com>
2017-10-10 21:35:26 +0200

    car update

--- database/src/main/java/com/nautilus/domain/Car.java
+++ database/src/main/java/com/nautilus/domain/Car.java
@@ -3,6 +3,7 @@ package com.nautilus.domain;
 
 import com.nautilus.constants.CarStatus;
 import com.nautilus.dto.car.CarRegisterDTO;
+import com.nautilus.dto.car.CarUpdateDTO;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -46,5 +47,58 @@ public class Car {
         this.color = carRegisterDTO.getColor();
         this.yearOfProduction = carRegisterDTO.getYearOfProduction();
         this.description = carRegisterDTO.getDescription();
+    }
+
+    public static Car mergeWithUpdateDto(Car car, CarUpdateDTO carUpdateDTO){
+        Car updatedCar = new Car();
+
+        updatedCar.setBeaconId(car.getBeaconId());
+
+        if(carUpdateDTO.getRegisterNumber() != null){
+            updatedCar.setRegisterNumber(carUpdateDTO.getRegisterNumber());
+        } else {
+            updatedCar.setRegisterNumber(car.getRegisterNumber());
+        }
+
+        if(carUpdateDTO.getMark() != null){
+            updatedCar.setMark(carUpdateDTO.getMark());
+        } else {
+            updatedCar.setMark(car.getMark());
+        }
+
+        if(carUpdateDTO.getModel() != null){
+            updatedCar.setModel(carUpdateDTO.getModel());
+        } else {
+            updatedCar.setModel(car.getModel());
+        }
+
+        if(carUpdateDTO.getColor() != null){
+            updatedCar.setColor(carUpdateDTO.getColor());
+        } else {
+            updatedCar.setColor(car.getColor());
+        }
+
+        if(carUpdateDTO.getYearOfProduction() != null){
+            updatedCar.setYearOfProduction(carUpdateDTO.getYearOfProduction());
+        } else {
+            updatedCar.setYearOfProduction(car.getYearOfProduction());
+        }
+
+        if(carUpdateDTO.getDescription() != null){
+            updatedCar.setDescription(carUpdateDTO.getDescription());
+        } else {
+            updatedCar.setDescription(car.getDescription());
+        }
+
+        if(carUpdateDTO.getStatus() != null){
+            updatedCar.setStatus(carUpdateDTO.getStatus());
+        } else {
+            updatedCar.setStatus(car.getStatus());
+        }
+
+        updatedCar.setOwner(car.getOwner());
+        updatedCar.setStatusSnapshots(car.getStatusSnapshots());
+
+        return updatedCar;
     }
 }
--- web/src/main/java/com.nautilus/rest/controllers/car/UpdateCarController.java
+++ web/src/main/java/com.nautilus/rest/controllers/car/UpdateCarController.java
@@ -1,6 +1,10 @@
 package com.nautilus.rest.controllers.car;
 
+import com.nautilus.domain.Car;
 import com.nautilus.dto.car.CarRegisterDTO;
+import com.nautilus.dto.car.CarUpdateDTO;
+import com.nautilus.services.def.GlobalService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,8 +17,19 @@ import javax.validation.Valid;
 @RequestMapping(value = "${car.update}")
 public class UpdateCarController {
 
+    @Autowired
+    private GlobalService service;
+
     @RequestMapping(method = RequestMethod.POST)
-    public HttpStatus update(@RequestBody @Valid CarRegisterDTO updateDto) {
+    public HttpStatus update(@RequestBody @Valid CarUpdateDTO updateDto) {
+
+        Car car = service.findCarByBeaconId(updateDto.getBeaconId());
+
+        if(car != null) {
+            Car updateCar = Car.mergeWithUpdateDto(car, updateDto);
+            service.save(updateCar);
+        }
+
         return HttpStatus.OK;
     }
 }
