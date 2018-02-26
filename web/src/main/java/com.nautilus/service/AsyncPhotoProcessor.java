d60ff3318fcd74c1785cca4a70c6925e54f56f3f
Yurii Piets <yurii.piets@gmail.com>
2018-02-26 21:22:10 +0100

    enabled async config

---
+++ web/src/main/java/com.nautilus/service/AsyncPhotoProcessor.java
@@ -0,0 +1,12 @@
+package com.nautilus.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+@Async
+@Service
+@RequiredArgsConstructor
+public class AsyncPhotoProcessor {
+
+}
