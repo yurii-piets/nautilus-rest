6732c0705ea6f874b358c739131d8be122966bdd
Yurii Piets <yurii.piets@gmail.com>
2018-03-03 19:53:39 +0100

    Controllers Test | index test with mock user

--- web/src/test/java/com/nautilus/rest/controllers/IndexControllerTest.java
+++ web/src/test/java/com/nautilus/rest/controllers/IndexControllerTest.java
@@ -5,6 +5,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -25,6 +26,13 @@ public class IndexControllerTest {
     public void indexNoAuthTest() throws Exception {
         mockMvc.perform(get(IndexController.INDEX_MAPPING))
                 .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    @WithMockUser(username = "test", password = "test", roles = "USER")
+    public void indexAuthTest() throws Exception {
+        mockMvc.perform(get(IndexController.INDEX_MAPPING))
+                .andExpect(status().isOk());
     }
 }
 
