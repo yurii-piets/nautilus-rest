bfabca1ef89257e7231fe874e4eddc9425b895a6
Yurii Piets <yurii.piets@gmail.com>
2018-03-05 21:20:51 +0100

    Controllers Test | post car tests

--- web/src/test/java/com/nautilus/rest/controllers/car/CarControllerTest.java
+++ web/src/test/java/com/nautilus/rest/controllers/car/CarControllerTest.java
@@ -5,6 +5,7 @@ import com.nautilus.constants.Authorities;
 import com.nautilus.constants.CarStatus;
 import com.nautilus.domain.Car;
 import com.nautilus.domain.UserConfig;
+import com.nautilus.dto.car.CarRegisterDTO;
 import com.nautilus.services.GlobalService;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -22,6 +23,7 @@ import java.util.LinkedHashSet;
 
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -76,6 +78,75 @@ public class CarControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(jsonUtil.getContentType()))
                 .andExpect(content().json(carJson));
+    }
+
+    @Test
+    @WithMockUser(username = MOCK_USER_EMAIL, password = MOCK_USER_PASSWORD, roles = "USER")
+    public void getCarStatusWhenCarIsOk() throws Exception {
+        when(service.findCarByBeaconId(MOCK_CAR_BEACON_ID)).thenReturn(mockCar);
+
+        mockMvc.perform(get(CarController.CAR_MAPPING + "/status/" + MOCK_CAR_BEACON_ID))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(jsonUtil.getContentType()))
+                .andExpect(content().json(jsonUtil.json(CarStatus.OK)));
+    }
+
+    @Test
+    public void postCarWhenNoAuth() throws Exception {
+        CarRegisterDTO carRegisterDTO = buildMockCarRegisterDTO();
+        String carJson = jsonUtil.json(carRegisterDTO);
+
+        mockMvc.perform(post(CarController.CAR_MAPPING)
+                .contentType(jsonUtil.getContentType())
+                .content(carJson))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    @WithMockUser(username = MOCK_USER_EMAIL, password = MOCK_USER_PASSWORD, roles = "USER")
+    public void postCarWhenCarFraud() throws Exception {
+        when(service.findUserConfigByEmail(MOCK_USER_EMAIL))
+                .thenReturn(mockUser);
+        when(service.findCarByBeaconIdOrRegisterNumber(mockCar.getBeaconId(), mockCar.getRegisterNumber()))
+                .thenReturn(mockCar);
+
+        CarRegisterDTO carRegisterDTO = buildMockCarRegisterDTO();
+        String carJson = jsonUtil.json(carRegisterDTO);
+
+        mockMvc.perform(post(CarController.CAR_MAPPING)
+                .contentType(jsonUtil.getContentType())
+                .content(carJson))
+                .andExpect(status().isNotAcceptable());
+    }
+
+    @Test
+    @WithMockUser(username = MOCK_USER_EMAIL, password = MOCK_USER_PASSWORD, roles = "USER")
+    public void postCar() throws Exception {
+        when(service.findUserConfigByEmail(MOCK_USER_EMAIL))
+                .thenReturn(mockUser);
+        when(service.findCarByBeaconIdOrRegisterNumber(mockCar.getBeaconId(), mockCar.getRegisterNumber()))
+                .thenReturn(null);
+
+        CarRegisterDTO carRegisterDTO = buildMockCarRegisterDTO();
+        String carJson = jsonUtil.json(carRegisterDTO);
+
+        mockMvc.perform(post(CarController.CAR_MAPPING)
+                .contentType(jsonUtil.getContentType())
+                .content(carJson))
+                .andExpect(status().isOk());
+    }
+
+    private CarRegisterDTO buildMockCarRegisterDTO() {
+        CarRegisterDTO carRegisterDTO = new CarRegisterDTO();
+        carRegisterDTO.setBeaconId(mockCar.getBeaconId());
+        carRegisterDTO.setColor(mockCar.getColor());
+        carRegisterDTO.setDescription(mockCar.getDescription());
+        carRegisterDTO.setMark(mockCar.getMark());
+        carRegisterDTO.setModel(mockCar.getModel());
+        carRegisterDTO.setRegisterNumber(mockCar.getRegisterNumber());
+        carRegisterDTO.setYearOfProduction(mockCar.getYearOfProduction());
+
+        return carRegisterDTO;
     }
 
     private static Car buildMockCar() {
