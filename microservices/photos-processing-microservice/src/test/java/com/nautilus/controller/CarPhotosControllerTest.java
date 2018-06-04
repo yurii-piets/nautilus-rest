710860c16be39f98f597abcf3a3924c1329d1090
Yurii Piets <yurii.piets@gmail.com>
2018-06-04 23:36:47 +0200

    Photo Processing Microservice | tested all delete methods

--- microservices/photos-processing-microservice/src/test/java/com/nautilus/controller/CarPhotosControllerTest.java
+++ microservices/photos-processing-microservice/src/test/java/com/nautilus/controller/CarPhotosControllerTest.java
@@ -22,6 +22,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.List;
 import java.util.Random;
 import java.util.Set;
@@ -38,6 +39,7 @@ import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.fileUpload;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -92,13 +94,13 @@ public class CarPhotosControllerTest {
     }
 
     @Test
-    public void getCarWhenNoAuth() throws Exception {
+    public void getCarPhotosWhenNoAuth() throws Exception {
         mockMvc.perform(get(CAR_PHOTOS_MAPPING.replace("{beaconId}", MOCK_CAR_BEACON_ID))).andExpect(status().isUnauthorized());
     }
 
     @Test
     @WithMockUser(username = MOCK_USER_EMAIL, password = MOCK_USER_PASSWORD, roles = "USER")
-    public void getCarWhenNotExist() throws Exception {
+    public void getCarPhotosWhenNotExist() throws Exception {
         doThrow(WrongBeaconIdException.class).when(dataService).checkIfCarExistByBeaconId(anyString());
 
         mockMvc.perform(get(CAR_PHOTOS_MAPPING.replace("{beaconId}", MOCK_CAR_BEACON_ID)))
@@ -125,7 +127,7 @@ public class CarPhotosControllerTest {
 
     @Test
     @WithMockUser(username = MOCK_USER_EMAIL, password = MOCK_USER_PASSWORD, roles = "USER")
-    public void getCarWhenIsOk() throws Exception {
+    public void getCarPhotosWhenIsOk() throws Exception {
         doNothing().when(dataService).checkIfCarExistByBeaconId(anyString());
         when(dataService.getCarStatusByCarBeaconId(MOCK_CAR_BEACON_ID)).thenReturn(CarStatus.OK);
         when(fileUtil.getOriginalIndices(MOCK_CAR_BEACON_ID)).thenReturn(List.of(1, 2, 3, 4));
@@ -503,8 +505,8 @@ public class CarPhotosControllerTest {
         doThrow(WrongBeaconIdException.class).when(dataService).checkIfCarExistByBeaconId(MOCK_CAR_BEACON_ID);
 
         mockMvc.perform(fileUpload(CAR_PHOTOS_MAPPING.replace("{beaconId}", MOCK_CAR_BEACON_ID))
-                        .file(new MockMultipartFile("file", Files.toByteArray(testImageFile)))
-                        .contentType("multipart/form-data"))
+                .file(new MockMultipartFile("file", Files.toByteArray(testImageFile)))
+                .contentType("multipart/form-data"))
                 .andExpect(status().isNotFound());
     }
 
@@ -579,8 +581,8 @@ public class CarPhotosControllerTest {
         doThrow(WrongBeaconIdException.class).when(dataService).checkIfCarExistByBeaconId(MOCK_CAR_BEACON_ID);
 
         mockMvc.perform(fileUpload((CAR_PHOTOS_MAPPING + CAPTURES_MAPPING).replace("{beaconId}", MOCK_CAR_BEACON_ID))
-                        .file(new MockMultipartFile("file", Files.toByteArray(testImageFile)))
-                        .contentType("multipart/form-data"))
+                .file(new MockMultipartFile("file", Files.toByteArray(testImageFile)))
+                .contentType("multipart/form-data"))
                 .andExpect(status().isNotFound());
     }
 
@@ -593,5 +595,80 @@ public class CarPhotosControllerTest {
                 .file(new MockMultipartFile("file", Files.toByteArray(testImageFile)))
                 .contentType("multipart/form-data"))
                 .andExpect(status().isAccepted());
+    }
+
+    @Test
+    public void deleteCarPhotosWhenNoAuth() throws Exception {
+        mockMvc.perform(delete(CAR_PHOTOS_MAPPING.replace("{beaconId}", MOCK_CAR_BEACON_ID)))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    @WithMockUser(username = "wrong_user", password = "wrong_pass", roles = "USER")
+    public void deleteCarPhotosWhenNotAllowed() throws Exception {
+        mockMvc.perform(delete(CAR_PHOTOS_MAPPING.replace("{beaconId}", MOCK_CAR_BEACON_ID)))
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    @WithMockUser(username = MOCK_USER_EMAIL, password = MOCK_USER_PASSWORD, roles = "USER")
+    public void deleteCarPhotosWhenNotFound() throws Exception {
+        doThrow(WrongBeaconIdException.class).when(dataService).checkIfCarExistByBeaconId(MOCK_CAR_BEACON_ID);
+        when(dataService.getEmailByBeaconId(MOCK_CAR_BEACON_ID)).thenReturn(MOCK_USER_EMAIL);
+
+        mockMvc.perform(delete(CAR_PHOTOS_MAPPING.replace("{beaconId}", MOCK_CAR_BEACON_ID)))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    @WithMockUser(username = MOCK_USER_EMAIL, password = MOCK_USER_PASSWORD, roles = "USER")
+    public void deleteCarPhotosWhenOk() throws Exception {
+        when(dataService.getEmailByBeaconId(MOCK_CAR_BEACON_ID)).thenReturn(MOCK_USER_EMAIL);
+
+        mockMvc.perform(delete(CAR_PHOTOS_MAPPING.replace("{beaconId}", MOCK_CAR_BEACON_ID)))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void deleteCarPhotosByIndexWhenNoAuth() throws Exception {
+        mockMvc.perform(delete((CAR_PHOTOS_MAPPING + 7).replace("{beaconId}", MOCK_CAR_BEACON_ID)))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    @WithMockUser(username = "wrong_user", password = "wrong_pass", roles = "USER")
+    public void deleteCarPhotosByIndexWhenNotAllowed() throws Exception {
+        mockMvc.perform(delete((CAR_PHOTOS_MAPPING + "/" + 7).replace("{beaconId}", MOCK_CAR_BEACON_ID)))
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    @WithMockUser(username = MOCK_USER_EMAIL, password = MOCK_USER_PASSWORD, roles = "USER")
+    public void deleteCarPhotosByIndexWhenNotFound() throws Exception {
+        doThrow(WrongBeaconIdException.class).when(dataService).checkIfCarExistByBeaconId(MOCK_CAR_BEACON_ID);
+        when(dataService.getEmailByBeaconId(MOCK_CAR_BEACON_ID)).thenReturn(MOCK_USER_EMAIL);
+
+        mockMvc.perform(delete((CAR_PHOTOS_MAPPING + "/7").replace("{beaconId}", MOCK_CAR_BEACON_ID)))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    @WithMockUser(username = MOCK_USER_EMAIL, password = MOCK_USER_PASSWORD, roles = "USER")
+    public void deleteCarPhotosByIndexWhenIndexNotFound() throws Exception {
+        int index = new Random(0).nextInt();
+        doThrow(FileNotFoundException.class).when(fileUtil).delete(MOCK_CAR_BEACON_ID, index);
+        when(dataService.getEmailByBeaconId(MOCK_CAR_BEACON_ID)).thenReturn(MOCK_USER_EMAIL);
+
+        mockMvc.perform(delete((CAR_PHOTOS_MAPPING + "/" + index).replace("{beaconId}", MOCK_CAR_BEACON_ID)))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    @WithMockUser(username = MOCK_USER_EMAIL, password = MOCK_USER_PASSWORD, roles = "USER")
+    public void deleteCarPhotoByIndexWhenOk() throws Exception {
+        when(dataService.getEmailByBeaconId(MOCK_CAR_BEACON_ID)).thenReturn(MOCK_USER_EMAIL);
+
+        mockMvc.perform(delete((CAR_PHOTOS_MAPPING + "/" + 8).replace("{beaconId}", MOCK_CAR_BEACON_ID)))
+                .andExpect(status().isOk());
     }
 }
